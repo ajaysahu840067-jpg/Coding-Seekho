49350e2a5e7752ad93a28ceecab82bcbf5639bdd
Ajay Gupta <ajaysahu840067@gmail.com>
2026-09-15 23:46:58 +0530

    Event bubbling & capturing

--- javascript/Event Handling/first.js
+++ javascript/Event Handling/first.js
@@ -1,7 +1,12 @@
 let btn = document.querySelector('#btn');
-let dada = document.querySelector('#dada');
-let papa = document.querySelector('#papa');
-let child = document.querySelector('#child');
+// let dada = document.querySelector('#dada');
+// let papa = document.querySelector('#papa');
+// let child = document.querySelector('#child');
+
+btn.addEventListener('click',()=>{console.log('Yes')});
+//btn.addEventListener('click',()=>{console.log('No')});
+
+btn.removeEventListener('click',()=>{console.log('remove')});
 
 let counter = 0;
 
@@ -17,31 +22,31 @@ let counter = 0;
 // });
 
 
-dada.addEventListener('click',hello);
-function hello(event_object){
-    console.log('counter2 = ${counter++}'); // string literal || format string
-    console.log(event_object.target);
-    console.log(event_object.currentTarget);
-};
+// dada.addEventListener('click',hello);
+// function hello(event_object){
+//     console.log('counter2 = ${counter++}'); // string literal || format string
+//     console.log(event_object.target);
+//     console.log(event_object.currentTarget);
+// };
 
-papa.addEventListener('click',hello1);
-function hello1(event_object){
-    console.log('counter2 = ${counter++}'); // string literal || format string
-    console.log(event_object.target);
-    console.log(event_object.currentTarget);
-};
+// papa.addEventListener('click',hello1);
+// function hello1(event_object){
+//     console.log('counter2 = ${counter++}'); // string literal || format string
+//     console.log(event_object.target);
+//     console.log(event_object.currentTarget);
+// };
 
-btn.addEventListener('click',hello2);
-function hello2(event_object){
-    console.log('counter2 = ${counter++}'); // string literal || format string
-    console.log(event_object.target);
-    console.log(event_object.currentTarget);
-};
+// btn.addEventListener('click',hello2);
+// function hello2(event_object){
+//     console.log('counter2 = ${counter++}'); // string literal || format string
+//     console.log(event_object.target);
+//     console.log(event_object.currentTarget);
+// };
 
-child.addEventListener('click',hello3);
-function hello3(event_object){
-    console.log('counter2 = ${counter++}'); // string literal || format string
-    console.log(event_object.target);
-    console.log(event_object.currentTarget);
-    event_object.stopPropagation();
-};
+// child.addEventListener('click',hello3);
+// function hello3(event_object){
+//     console.log('counter2 = ${counter++}'); // string literal || format string
+//     console.log(event_object.target);
+//     console.log(event_object.currentTarget);
+//     event_object.stopPropagation();
+// };
